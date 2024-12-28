f7336777cbcb890a9e5bc1e18009a6952f98e151
danik0n <gviner52@gmail.com>
2024-12-29 01:30:51 +0300

    transition to FSD

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the Home Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style scoped>
+
+</style>
